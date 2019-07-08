90d4f126d40a9ee697088eb62451b0425764a7a6
Tran Sang <tranvansangk41@gmail.com>
2019-07-08 23:43:56 +0900

    date.now mock test sample repo

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,14 @@
+const corejs = 3
 module.exports = api => {
   api.cache(true)
   return {
     presets: [
-      '@babel/preset-env',
-      '@babel/preset-typescript'
+      ['@babel/preset-env', {modules: 'commonjs', useBuiltIns: 'entry', corejs}],
+      ['@babel/preset-typescript', {isTSX: true, allExtensions: true}]
+    ],
+    plugins: [
+      //comment the following line will make test pass
+      ['@babel/plugin-transform-runtime', {corejs}]
     ]
   }
 }
--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,8 @@
 import 'regenerator-runtime'
-import flipPromise from './index'
 
-describe('promise flipper', () => {
-  it('should flip promise ok', async () => {
-    expect(await flipPromise(Promise.reject(1))).toEqual(1)
-    expect(await flipPromise(flipPromise(2))).toEqual(2)
-    expect(await flipPromise(flipPromise(Promise.resolve(3)))).toEqual(3)
+describe('date.now', () => {
+  it('mock', async () => {
+    Date.now = () => 1
+    expect(Date.now()).toBe(1)
   })
 })
